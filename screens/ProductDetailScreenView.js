671ce9ffbf03adf53071215857e6259ec6c97151
Damish-N <damishnisal100@gmail.com>
2021-10-10 10:42:06 +0530

    added to favourite list function

--- screens/ProductDetailScreenView.js
+++ screens/ProductDetailScreenView.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  Alert,
   Button,
   Image,
   ScrollView,
@@ -10,10 +11,89 @@ import {
 } from 'react-native';
 import Colors from '../constants/Colors';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import ButtonIconPapper from "../components/Button";
+import ButtonIconPapper from '../components/Button';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import firestore from '@react-native-firebase/firestore';
 
 const ProductDetailsView = ({route}) => {
-  const {product,productId} = route.params;
+  const [key, setKey] = useState('');
+  const [email, setEmail] = useState('');
+  const [listFav, setListFav] = useState([]);
+
+  useEffect(() => {
+    _retrieveData();
+  }, []);
+
+  const _retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('userName');
+      if (value !== null) {
+        // We have data!!
+        let val = typeof value;
+        let js = JSON.parse(value);
+        console.log('user name:1 ', js.user.email);
+        setEmail(js.user.email);
+        _getId(js.user.email);
+        // console.log(c);
+      }
+    } catch (error) {
+      console.log(error);
+      // Error retrieving data
+    }
+  };
+  const _getId = async e => {
+    const list = [];
+    let l;
+    console.log('email is', e);
+    firestore()
+      .collection('User')
+      .where('email', '==', e)
+      .get()
+      .then(querySnapshot => {
+        console.log('Total users:', querySnapshot.size);
+
+        querySnapshot.forEach(documentSnapshot => {
+          console.log(
+            'User ID: ',
+            documentSnapshot.id,
+            documentSnapshot.data(),
+          );
+          setKey(documentSnapshot.id);
+          if (documentSnapshot.data().listOfFav.length > 0) {
+            documentSnapshot.data().listOfFav.forEach(e => {
+              list.push(e);
+            });
+            setListFav(list);
+          }
+        });
+      });
+  };
+  const _updateDetailes = async => {
+    // console.log({listFav});
+    firestore()
+      .collection('User')
+      .doc(key)
+      .update({
+        listOfFav: [...listFav, productId],
+      })
+      .then(() => {
+        // setLoadingUpdate(false);
+        Alert.alert(
+          'Added to Favourite',
+          'Successfully added to favourite list',
+          [
+            {
+              text: 'Okey',
+              onPress: () => {
+                console.log('okey');
+              },
+            },
+          ],
+        );
+        console.log('User updated!');
+      });
+  };
+  const {product, productId} = route.params;
   return (
     <ScrollView>
       <Image source={{uri: product.imageUrl}} style={styles.image} />
@@ -23,7 +103,25 @@ const ProductDetailsView = ({route}) => {
             color={Colors.primary}
             title="Add to favourite"
             onPress={() => {
-              console.log('Added tp the Cart');
+              console.log('Unable to add favourite');
+              if (listFav.includes(productId)) {
+                Alert.alert(
+                  'Unable to add favourite',
+                  'Already Exits in the favourite list ',
+                  [
+                    {
+                      text: 'yes',
+                      onPress: () => {
+                        console.log('okey');
+                      },
+                    },
+                  ],
+                );
+              } else {
+                setListFav(prevState => [...prevState, productId]);
+                console.log(listFav);
+                _updateDetailes();
+              }
             }}
             style={styles.btn}
           />
@@ -31,6 +129,11 @@ const ProductDetailsView = ({route}) => {
         <Text style={styles.price}>Rs:{product.price}</Text>
         <Text style={styles.description}>{product.description}</Text>
         <Text style={styles.description}>{productId}</Text>
+        {listFav.length > 0 ? (
+          listFav.map(e => <Text key={e}>{e}</Text>)
+        ) : (
+          <Text>no Data </Text>
+        )}
       </View>
       <View>
         {/*<TouchableOpacity>*/}
